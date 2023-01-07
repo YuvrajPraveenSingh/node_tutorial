a74bbc7ba81a7d60e18bce9f3a5a90f254ef526a
YuvrajPraveenSingh <yuv00191@outlook.com>
2023-01-07 17:13:23 +0530

    02_Gettinginput

---
+++ src/02_GettingInput/index.ts
@@ -0,0 +1,9 @@
+import  fs  from "fs";
+
+const input = process.argv;
+
+if(input[2] == 'add') fs.writeFileSync(input[3], input[4]);
+else if(input[2] == 'remove') fs.unlinkSync(input[3]);
+else console.log("invalid input")
+
+
